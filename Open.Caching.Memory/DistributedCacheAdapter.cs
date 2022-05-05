c7316fef979e4d8d85961ba869d443d68e85f7bd
electricessence <5899455+electricessence@users.noreply.github.com>
2022-05-05 13:38:26 -0700

    Parking changes.

---
+++ Open.Caching.Memory/DistributedCacheAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Open.Caching.Memory;
+
+public class DistributedCacheAdapter<TKey> : IDistributedCache
+{
+	public byte[] Get(string key) => throw new NotImplementedException();
+	public Task<byte[]> GetAsync(string key, CancellationToken token = default) => throw new NotImplementedException();
+	public void Refresh(string key) => throw new NotImplementedException();
+	public Task RefreshAsync(string key, CancellationToken token = default) => throw new NotImplementedException();
+	public void Remove(string key) => throw new NotImplementedException();
+	public Task RemoveAsync(string key, CancellationToken token = default) => throw new NotImplementedException();
+	public void Set(string key, byte[] value, DistributedCacheEntryOptions options) => throw new NotImplementedException();
+	public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token = default) => throw new NotImplementedException();
+}
---
+++ Open.Caching/IAsyncCacheAdapter.cs
@@ -0,0 +1,36 @@
+﻿namespace Open.Caching;
+
+public interface IAsyncCacheAdapter<TKey>
+{
+	/// <summary>
+	/// Gets the item associated with this key if present.
+	/// </summary>
+	/// <param name="key">A <typeparamref name="TKey"/> identifying the requested item.</param>
+	/// <param name="throwIfUnexpectedType"></param>
+	/// <returns>True if the key was found; otherwise false.</returns>
+	/// <exception cref="InvalidCastException">
+	/// If <paramref name="throwIfUnexpectedType"/> is true
+	/// and the value does not resolve to the expected type.
+	/// </exception>
+	/// <returns>
+	/// The result of this async method is a Task of <typeparamref name="TValue"/>.
+	/// If the item is found, a Task is returned containing the value.
+	/// If the item is not availalbe, null is returned.
+	/// </returns>
+	ValueTask<Task<TValue>?> GetValueAsync<TValue>(
+		TKey key,
+		bool throwIfUnexpectedType = false);
+
+	/// <summary>
+	/// Adds or overwrites an item in the cache.
+	/// </summary>
+	/// <param name="key">A <typeparamref name="TKey"/> identifying the item.</param>
+	/// <param name="item">The item to insert.</param>
+	ValueTask SetAsync<TValue>(TKey key, TValue item);
+
+	/// <summary>
+	/// Removes an item from the cache.
+	/// </summary>
+	/// <param name="key"></param>
+	ValueTask RemoveAsync(TKey key);
+}
